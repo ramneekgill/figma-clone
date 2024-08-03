3855d07db73d404322f4a771a951cbfc49722963
Ramneek Gill <ramneekg25@gmail.com>
2024-08-03 15:15:59 -0600

    added reaction selector and flying reactions on my screen and other users screen

--- components/reaction/FlyingReaction.tsx
+++ components/reaction/FlyingReaction.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import styles from "./index.module.css";
 
-const FlyingReaction = () => {
-  return <div>FlyingReaction</div>;
+type Props = {
+  x: number;
+  y: number;
+  timestamp: number;
+  value: string;
 };
 
-export default FlyingReaction;
+export default function FlyingReaction({ x, y, timestamp, value }: Props) {
+  return (
+    <div
+      className={`pointer-events-none absolute select-none ${
+        styles.disappear
+      } text-${(timestamp % 5) + 2}xl ${styles["goUp" + (timestamp % 3)]}`}
+      style={{ left: x, top: y }}
+    >
+      <div className={styles["leftRight" + (timestamp % 3)]}>
+        <div className="-translate-x-1/2 -translate-y-1/2 transform">
+          {value}
+        </div>
+      </div>
+    </div>
+  );
+}
